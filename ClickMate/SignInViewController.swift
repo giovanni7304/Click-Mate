1ed23f1c43230095017117c2d7ebef8ce2719b63
Terry Johnson <giovanni7304@me.com>
2016-10-03 22:20:59 -0500

    Initial Commit	with FireBase

---
+++ ClickMate/SignInViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SignInViewController.swift
+//  ClickMate
+//
+//  Created by Terry Johnson on 10/3/16.
+//  Copyright © 2016 Terry Johnson. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("ViewController Loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func trymeTapped(_ sender: AnyObject) {
+        
+        
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            print("we tried to sign in")
+            if error != nil {
+                print("hey we have an error: \(error)")
+                
+                FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    print("We tried to create user")
+                    if error != nil {
+                        print("Hey we have and error:\(error)")
+                    } else {
+                        print("Signed in Successfully")
+                    }
+                })
+            } else {
+                print("hey we have signed in success")
+            }
+        })
+        
+    }
+
+}
+
